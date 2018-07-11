e8583062026eb744be1ed7728cfc1ab6ab4703a4
jacob <jbutler8@terpmail.umd.edu>
2018-07-11 15:27:40 -0700

    add hero list; selected element editability; styling

--- src/app/characters/characters.component.ts
+++ src/app/characters/characters.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-characters',
@@ -7,12 +8,14 @@ import { Hero } from '../hero';
   styleUrls: ['./characters.component.scss']
 })
 export class CharactersComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  selectedHero: Hero;
+  heroes = HEROES;
 
   constructor() { }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 
   ngOnInit() {
   }
