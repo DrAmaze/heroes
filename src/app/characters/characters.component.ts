b8106e432f498b83a8881895c401a902a2d8e2c8
jacob <jbutler8@terpmail.umd.edu>
2018-07-11 14:10:16 -0700

    add hero module

---
+++ src/app/characters/characters.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+
+@Component({
+  selector: 'app-characters',
+  templateUrl: './characters.component.html',
+  styleUrls: ['./characters.component.scss']
+})
+export class CharactersComponent implements OnInit {
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
